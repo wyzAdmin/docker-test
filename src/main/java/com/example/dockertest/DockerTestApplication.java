0654c1ae91032d95c07698d56a3ac268739c695c
wyz <yaozong.wu@foxmail.com>
2019-10-11 22:00:51 +0800

    111

--- src/main/java/com/example/dockertest/DockerTestApplication.java
+++ src/main/java/com/example/dockertest/DockerTestApplication.java
@@ -1,13 +1,11 @@
 package com.example.dockertest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DockerTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DockerTestApplication.class, args);
+            
+
     }
 
 }
