aea2aecfe805c6f8fc8b21cf9461316dd67b3809
wyz <yaozong.wu@foxmail.com>
2019-10-13 19:15:36 +0800

    222

--- src/main/java/com/example/dockertest/DockerTestApplication.java
+++ src/main/java/com/example/dockertest/DockerTestApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerTestApplication {
 
     public static void main(String[] args) {
-
+  ///
         SpringApplication.run(DockerTestApplication.class, args);
     }
 
